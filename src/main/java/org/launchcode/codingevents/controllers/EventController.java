0f78d64ef3a642af6701d7c516aba4cb04969eeb
maxwell.maier <maxwellmaier89@gmail.com>
2024-05-02 18:48:45 -0500

    completed alongside phillip during studio

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,16 +8,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
 @RequestMapping("/events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+    private static HashMap<String, String> events = new HashMap();
 
     @GetMapping
     public String getEvents(Model model) {
+        events.put("Menteaship","A fun meetup for connecting with mentors");
+        events.put("Code With Pride","A fun meetup sponsored by LaunchCode");
+        events.put("Javascripty", "An imaginary meetup for Javascript developers");
+
+
+
         model.addAttribute("events", events);
         return "events/index";
     }
@@ -29,8 +36,8 @@ public class EventController {
 
     // lives at /events/create
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName){
-        events.add(eventName);
+    public String createEvent(@RequestParam String eventName, String eventDescription){
+        events.put(eventName, eventDescription);
         return "redirect:/events";
     }
 
